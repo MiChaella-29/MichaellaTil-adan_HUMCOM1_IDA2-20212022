5d8753140203ce7fa011374a5b2c94866bac3933
Michaella Til-adan <97874629+MiChaella-29@users.noreply.github.com>
2022-04-28 21:51:47 +0800

    Add files via upload

---
+++ EXTERNAL SCRIPT.js
@@ -0,0 +1,26 @@
+<DOCTYPE HTML>
+	<HTML>
+	<HEAD>
+		<TITLE>uSING ALERT</TITLE>
+      </HEAD>
+	 
+   <BODY>
+		<h2 id= "p1"> HELLO WORLD!!!</h2>
+		<h2 id= "p2"> HELLO WORLD!!!</h2>
+	 <SCRIPT>
+		ALERT("MICHAELLA TIL-ADAN.\nBS Information Technology.\N1ST Year.\nHUMCOM1-IDA2.\nHUMAN COMPUTER INTERACTION");
+	 </SCRIPT>
+	 <SCRIPT>
+       CONSOLE.LOG("GOODBYE");
+     CONSOLE.LOG(5+12);
+    </SCRIPT>
+     <SCRIPT>
+	DOCUMENT.WRITE("GOODBYE WORLD");
+      document.getElementById("p2").style.color = "blue";
+     document.getElementById("p2").style.fontFamily = "Arial";
+      document.getElementById("p2").style.fontSize = "larger";
+     </SCRIPT>
+
+
+     </BODY>
+    </HTML>
