29948e9bbef861882ab12ffc46ccae4a96a86d49
Michaella Til-adan <97874629+MiChaella-29@users.noreply.github.com>
2022-05-12 18:39:44 +0800

    Add files via upload

---
+++ Exercise 15.js
@@ -0,0 +1,11 @@
+!DOCTYPEhtml>
+<html>
+<body>
+
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("INPUT 1").TEXTCONTENT = "MICHAELLA!"'>Click Me!</button>
+
+</body>
+</html>
